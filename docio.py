fb7dea5e14fc360183112bbde05d8dcc5a9bd791
zhangmeishan <mason.zms@gmail.com>
2025-10-02 21:27:55 +0800

    test

--- docio.py
+++ docio.py
@@ -1,19 +1,18 @@
-#from docling.document_converter import DocumentConverter
-
-#source = "https://arxiv.org/pdf/2408.09869"  # document per local path or URL
-#converter = DocumentConverter()
-#result = converter.convert(source)
-#print(result.document.export_to_markdown())  # output: "## Docling Technical Report[...]"
+## This file is for PDF io
+## either
 
 import cv2 as cv
 from pypdf import PdfReader
 from pdf2image import convert_from_path
+from PIL import Image
+import base64
 
 
 im_pages = convert_from_path("input/test.pdf", 1000)
 
 for count, page in enumerate(im_pages):
     page.show()
+    base64.b64encode(page).decode("utf-8")
 
 reader = PdfReader("input/test.pdf")
 number_of_pages = len(reader.pages)
